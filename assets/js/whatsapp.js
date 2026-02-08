fff1f504977a2b2b4bd52f46891d28236e7f9e45
brian <brian@PC-BRIGI>
2026-02-08 02:32:25 -0600

    Se agrega la funcionalidad reservación por mensaje de WhatsApp

---
+++ assets/js/whatsapp.js
@@ -0,0 +1,12 @@
+/**
+ * WhatsApp Reservation Functionality
+ */
+
+function reservarServicio(nombreServicio) {
+    const phoneNumber = '50672460288';
+    const message = `Hola, me gustaría hacer una reservación para ${nombreServicio}`;
+    const encodedMessage = encodeURIComponent(message);
+    const url = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`;
+
+    window.open(url, '_blank');
+}
